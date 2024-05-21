497303b7e9f7f2cb949c3037289afdb6d5d9b0f7
i-Ac1D-i <acidondiscord99@gmail.com>
2024-05-21 21:43:10 +0200

    Cookie di sessione, Registrazione, Inizio Dashboard, Organizzazione Componenti

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,81 @@
 import { Icon } from "design-react-kit"
+import { useEffect } from "react";
 export default function Header(){
+
+  
+  function setCookie(name, value, days = 1) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
+  function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+  
+  function hasCookies() {
+    return document.cookie.length > 0;
+  }
+  
+  function deleteCookie(name) {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
+
+  function validateCookies() {
+    const user = getCookie("username");
+    const expiration = getCookie("expiration");
+  
+    if (user && expiration) {
+      const expirationDate = new Date(expiration);
+      const now = new Date();
+  
+      if (now > expirationDate) {
+        deleteCookie("username");
+        deleteCookie("expiration");
+        window.location.href = "/login";
+        return false;
+      }
+  
+      return true;
+    }
+  
+    window.location.href = "/login";
+    return false;
+  }
+  
+  function getInfoFromCookies() {
+    const user = getCookie("username");
+    if (validateCookies()) {
+      return { username: user };
+    }
+    return null;
+  }
+  
+  // Usage of setCookie to include expiration date
+  function setSessionCookies(username) {
+    const now = new Date();
+    const expirationDate = new Date(now.getTime() + 24 * 60 * 60 * 1000).toUTCString();
+    setCookie("username", username, 1);
+    setCookie("expiration", expirationDate, 1);
+  }
+  
+  useEffect(() => {
+    if(location.pathname == "/login" || location.pathname == "/register") return
+    if (!getInfoFromCookies()) {
+      deleteCookie("username")
+      location.assign("/login")
+    } else {
+      setSessionCookies(getInfoFromCookies().username)
+    }
+  }, [])
+
     return (
         <div className="it-header-wrapper">
             <div className="it-nav-wrapper">
